20c3900b846271a796d4630406fe17a9168704ca
Shahidul <ishahin56@gmail.com>
2022-09-28 00:55:24 +0600

    Fixed paper section width issue

--- src/Layouts/PaperSection.js
+++ src/Layouts/PaperSection.js
@@ -6,26 +6,28 @@ import paperTwo from '../assets/images/PaperTwo.png';
 const PaperSection = () => {
     return (
         <div className="pt-24 pb-30 md:pb-60 lg:pb-80 px-16 bg-paper-section">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-10 xl:gap-16">
-                <div className="flex items-center justify-center">
-                    <img src={paperGreen} alt="Paper" />
+            <div className="max-w-[1200px] mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-10 xl:gap-16">
+                    <div className="flex items-center justify-center">
+                        <img src={paperGreen} alt="Paper" />
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <img src={paperGreen2} alt="Paper" />
+                    </div>
                 </div>
-                <div className="flex items-center justify-center">
-                    <img src={paperGreen2} alt="Paper" />
-                </div>
-            </div>
-            <div className="max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 md:gap-10 xl:gap-16">
-                <div className="flex items-center justify-center mt-3">
-                    <img src={paperTwo} alt="Paper" />
-                </div>
-                <div className="flex items-center justify-center mt-3">
-                    <img src={paperTwo} alt="Paper" />
-                </div>
-                <div className="flex items-center justify-center mt-3">
-                    <img src={paperTwo} alt="Paper" />
-                </div>
-                <div className="flex items-center justify-center mt-3">
-                    <img src={paperTwo} alt="Paper" />
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 md:gap-10 xl:gap-16">
+                    <div className="flex items-center justify-center mt-3">
+                        <img src={paperTwo} alt="Paper" />
+                    </div>
+                    <div className="flex items-center justify-center mt-3">
+                        <img src={paperTwo} alt="Paper" />
+                    </div>
+                    <div className="flex items-center justify-center mt-3">
+                        <img src={paperTwo} alt="Paper" />
+                    </div>
+                    <div className="flex items-center justify-center mt-3">
+                        <img src={paperTwo} alt="Paper" />
+                    </div>
                 </div>
             </div>
         </div>
